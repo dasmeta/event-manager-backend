632439626b437b9945c19b166c871a4106e384e6
Vahe Chakhalyan <vahe@dasmeta.com>
2022-02-03 20:22:38 +0400

    EventController.js functionality

---
+++ app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '93cb7d5257c38f97bf5910da1c539f6c'),
+    },
+  },
+});
