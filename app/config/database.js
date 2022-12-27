fc29c37150bbb17ae9ee996ecff18d02b68b6ad1
hovhannes.babayan <hovhannes.babayan@fouraitch.com>
2022-12-27 11:15:10 +0400

    fix postgres integration

--- app/config/database.js
+++ app/config/database.js
@@ -13,8 +13,14 @@ module.exports = ({ env }) => {
         connector: client === 'postgres' ? 'bookshelf' : 'mongoose',
         settings: {
           client,
+          host: env('DATABASE_HOST', 'mongo'),
+          port: env.int('DATABASE_PORT', client === 'postgres' ? 5432 : 27017),
+          database: env('DATABASE_NAME', 'strapi'),
+          username: env('DATABASE_USERNAME', 'strapi'),
+          password: env('DATABASE_PASSWORD', 'strapi'),
           uri: env("DATABASE_URL"),
-          // autoIndex: false,
+          ssl: env("DATABASE_SSL", false),
+          autoIndex: false,
         },
         options: {
           // authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
